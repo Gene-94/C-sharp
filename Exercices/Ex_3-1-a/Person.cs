e17ecabf5687279bf5e3dfa5c635e16ba70791b0
Yevheniy <yevheniykushnirenko@gmail.com>
2022-07-21 21:35:00 +0100

    Simple Exercice to get familiar with auto-properties

---
+++ Exercices/Ex_3-1-a/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_3_1_a
+{
+    public class Person
+    {
+        public string? Name {get; set;}
+        public string? Surname {get; set;}
+        public string? Age {get; set;}
+    }
+}
--- Exercices/Ex_3-1-a/Program.cs
+++ Exercices/Ex_3-1-a/Program.cs
@@ -3,6 +3,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Person person = new();
+
+        Console.WriteLine("\nHello !\n");
+        Console.Write("What is your name?\n> ");
+        person.Name = Console.ReadLine();
+        Console.Write("\nWhat is your surname?\n> ");
+        person.Surname = Console.ReadLine();
+        Console.Write("\nWhat is your age?\n> ");
+        person.Age = Console.ReadLine();
+
+        Console.Write("\nNice to meet you {0} {1}. I see you are {2} years wise\n", person.Name, person.Surname, person.Age);
+
     }
 }
