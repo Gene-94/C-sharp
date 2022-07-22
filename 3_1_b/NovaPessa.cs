3d2c659c306ea9ac7f5a4d80521f507a82a5ddeb
Yevheniy <yevheniykushnirenko@gmail.com>
2022-07-22 08:25:37 +0100

    referenced project Ex_3-1-a

---
+++ 3_1_b/NovaPessa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _3_1_a;
+
+namespace _3_1_b
+{
+    public class NovaPessa : Pessoa
+    {
+        public int Idade{set; get;}
+    }
+}
