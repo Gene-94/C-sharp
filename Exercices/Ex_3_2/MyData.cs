d7660fd721bdfc83505a73a85668253dbb8ebee4
Yevheniy <yevheniykushnirenko@gmail.com>
2022-07-23 00:22:39 +0100

    Datatypes validation and conversion practice

---
+++ Exercices/Ex_3_2/MyData.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_3_2
+{
+    public class MyData
+    {
+        public int myInt{set; get;}
+
+        public string? myString{set; get;}
+
+        public float myFloat{set; get;}
+    
+        public bool myBool{set; get;}
+
+        public ushort myUnsignedShort{set; get;}
+
+        public decimal myDecimal{set; get;}
+
+        public double myDouble{set; get;}
+    
+    }
+}
--- Exercices/Ex_3_2/Program.cs
+++ Exercices/Ex_3_2/Program.cs
@@ -1,8 +1,48 @@
-﻿namespace Ex_3_2;
+﻿using System.Globalization;
+namespace Ex_3_2;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CultureInfo culture = new CultureInfo("pt-PT");  
+
+        MyData data = new();
+
+        Console.Write("What is you favorite year?\n> ");
+        while(true){
+            if(ushort.TryParse(Console.ReadLine(), out var tmp)){
+                data.myUnsignedShort = tmp;
+                break;
+            }
+            Console.WriteLine("Please enter a valid year!");
+        }
+        Console.Write("What was the best thing that happened that year?\n> ");
+        data.myString = Console.ReadLine();
+        Console.Write("Did you buy something you really liked that year?\n> ");
+        while(true){
+            if(Boolean.TryParse(Console.ReadLine(), out var tmp)){
+                data.myBool = tmp;
+                break;
+            }
+            Console.WriteLine("Please answer with 'true' or 'false'");
+        }
+        while(data.myBool){
+            Console.Write("How much did it cost?\n> ");
+            if(float.TryParse(Console.ReadLine(), out var tmp)){
+                if(tmp>=0){
+                    data.myFloat=tmp;
+                    break;
+                }
+            }
+            Console.WriteLine("Insert a valid value for the item price.");
+        }
+        while(true){
+            Console.Write("When were you born?\n(dd/mm/yyyy) > ");
+            if(DateTime.TryParse(Console.ReadLine(), out var date)){
+                data.myDouble = (DateTime.Now).Subtract(date).TotalSeconds;
+                break;
+            }
+            Console.WriteLine("Write a valid date!");
+        }
     }
 }
