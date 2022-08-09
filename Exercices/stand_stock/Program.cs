2dc20b36abefedaf3c19edddda6b854f2aa1bb32
Gene-94 <yevheniykushnirenko@gmail.com>
2022-08-09 15:17:53 +0100

    writing to file with FileStream, overwriting content as it writes

--- Exercices/stand_stock/Program.cs
+++ Exercices/stand_stock/Program.cs
@@ -1,8 +1,19 @@
-﻿namespace stand_stock;
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace stand_stock;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        FileStream fs = new FileStream("stock.csv", FileMode.Open, FileAccess.ReadWrite);
+
+        //string? message = "test";
+
+        byte[] message = new UTF8Encoding(true).GetBytes("test");
+        fs.Write(message);
+        fs.Close();
     }
 }
